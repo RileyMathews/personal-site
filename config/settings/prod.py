99ceec10f82d0bf573c53ba1e605d8c7bbea3ae5
Riley Mathews <rileymathews80@gmail.com>
2022-12-15 22:17:19 -0600

    ran black formatting

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,9 +3,7 @@ import dj_database_url
 
 DEBUG = False
 ALLOWED_HOSTS = ["rileymathews.fly.dev"]
-DATABASES = {
-    "default": dj_database_url.parse(get_env_variable("DATABASE_URL"))
-}
+DATABASES = {"default": dj_database_url.parse(get_env_variable("DATABASE_URL"))}
 SECRET_KEY = get_env_variable("SECRET_KEY")
 CSRF_COOKIE_DOMAIN = "rileymathews.fly.dev"
 CSRF_TRUSTED_ORIGINS = ["https://rileymathews.fly.dev"]
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.shortcuts import HttpResponse
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', lambda request: HttpResponse("Hello World")),
+    path("admin/", admin.site.urls),
+    path("", lambda request: HttpResponse("Hello World")),
 ]
