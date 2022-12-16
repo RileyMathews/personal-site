6016c5a97b980bcd3df48193276975dde9285b39
Riley Mathews <rileymathews80@gmail.com>
2022-12-15 21:13:20 -0600

    added basic root page for debugging

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: HttpResponse("Hello World")),
 ]
