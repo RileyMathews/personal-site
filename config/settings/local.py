decdbb4bf93714b688ed3b81987310983c6b030b
Riley Mathews <rileymathews80@gmail.com>
2022-12-15 21:41:25 -0600

    added database config

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,3 +3,6 @@ import dj_database_url
 
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
+DATABASES = {
+    "default": dj_database_url.parse("postgres://user:password@database:5432/database")
+}
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,9 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 ALLOWED_HOSTS = ["rileymathews.fly.dev"]
+DATABASES = {
+    "default": dj_database_url.parse(get_env_variable("DATABASE_URL"))
+}
+SECRET_KEY = get_env_variable("SECRET_KEY")
