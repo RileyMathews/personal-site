a289451fc19dbf054927858f198752615ced59ec
Riley Mathews <rileymathews80@gmail.com>
2022-12-23 16:52:39 -0600

    added model for project page

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from taggit.models import TaggedItemBase
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
 
 
 class HomePage(Page):
@@ -10,4 +13,25 @@ class HomePage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel("body"),
+    ]
+
+class TechnologyTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'ProjectPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE,
+    )
+
+class ProjectPage(Page):
+    description = RichTextField(blank=True)
+    code_repository_url = models.URLField(blank=True)
+    technologies = ClusterTaggableManager(through=TechnologyTag, blank=True)
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+        FieldPanel("code_repository_url"),
+        MultiFieldPanel([
+            FieldPanel('technologies')
+        ])
     ]
