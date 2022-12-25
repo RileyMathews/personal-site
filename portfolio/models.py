8c2844752dbccc44630d9fac446eaa9f5d56b84a
Riley Mathews <rileymathews80@gmail.com>
2022-12-25 17:05:46 -0600

    added project pages

--- portfolio/models.py
+++ portfolio/models.py
@@ -12,4 +12,23 @@ class PortfolioIndexPage(Page):
     ]
 
     parent_page_types = ['home.HomePage']
+    subpage_types = ['portfolio.ProjectPage']
+
+    def get_context(self, request, *args, **kwargs):
+        context = super(PortfolioIndexPage, self).get_context(request, *args, **kwargs)
+
+        context['project_pages'] = self.get_children().live().specific()
+        return context
+
+
+class ProjectPage(Page):
+    description = RichTextField()
+    repository_url = models.URLField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('description'),
+        FieldPanel('repository_url'),
+    ]
+
+    parent_page_types = ['portfolio.PortfolioIndexPage']
     subpage_types = []
