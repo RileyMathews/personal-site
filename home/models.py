4543258a088973be717a9178dc462ef195c503c8
Riley Mathews <rileymathews80@gmail.com>
2022-12-24 20:47:53 -0600

    added basic home page

---
+++ home/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+# Create your models here.
+class HomePage(Page):
+    body = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body")
+    ]
