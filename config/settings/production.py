ff59ed68e24c0bee8cf41abfe4073ec4ceb8be85
Riley Mathews <rileymathews80@gmail.com>
2023-08-08 17:40:15 -0500

    add csrf variables

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,6 +4,8 @@ import dj_database_url
 DEBUG = False
 SECRET_KEY = get_env_variable("SECRET_KEY")
 ALLOWED_HOSTS = ["test.home.rileymathews.com"]
+CSRF_COOKIE_DOMAIN="test.home.rileymathews.com"
+CSRF_TRUSTED_ORIGINS=["test.home.rileymathews.com"]
 DATABASES = {
     "default": dj_database_url.parse(get_env_variable("DATABASE_URL")),
 }
